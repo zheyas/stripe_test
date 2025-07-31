41b3952e19968de3bd779e46c08dfa960e406d3b
Евгений <zhenyayasakov@yandex.ru>
2025-07-31 17:36:45 +0400

    add readme

--- discounts/urls.py
+++ discounts/urls.py
@@ -7,5 +7,4 @@ urlpatterns = [
     path('discounts/', DiscountListView.as_view(), name='discounts-list'),
     path('create/', views.create_discount, name='create_discount'),
     path('<int:pk>/', views.discount_detail, name='discount_detail'),
-
 ]
